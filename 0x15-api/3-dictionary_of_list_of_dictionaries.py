0b940e2115c34ad87df28d5d1d8846ef55820937
ab3lT <72024981+ab3lT@users.noreply.github.com>
2022-04-11 23:42:13 +0300

    Fixing the error

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
 """
-extend your Python script to export data in the JSON format
+    Python script to export data in the JSON format
+    Records all tasks from all employees
 """
-
 import json
 import requests
 
 if __name__ == '__main__':
-    users = requests.get("http://jsonplaceholder.typicode.com/users",
-                         verify=False).json()
-    userdict = {}
-    usernamedict = {}
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    users = requests.get('{}users'.format(url)).json()
+    records = {}
+    name = {}
+
     for user in users:
-        uid = user.get("id")
-        userdict[uid] = []
-        usernamedict[uid] = user.get("username")
-    todo = requests.get("http://jsonplaceholder.typicode.com/todos",
-                        verify=False).json()
+        Employee_id = user.get("id")
+        records[Employee_id] = []
+        name[Employee_id] = user.get("username")
 
-    for task in todo:
-        taskdict = {
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": usernamedict.get(uid)
+    todos = requests.get('{}todos'.format(url)).json()
+    for todo in todos:
+        Employee_id = todo.get("userId")
+        todo_obj = {
+            "username": name.get(Employee_id),
+            "task": todo.get('title'),
+            "completed": todo.get('completed')
         }
-        uid = task.get("userId")
+        records.get(Employee_id).append(todo_obj)
 
-        userdict.get(uid).append(taskdict)
-    with open("todo_all_employees.json", 'w') as jsonfile:
-        json.dump(userdict, jsonfile)
+    filename = 'todo_all_employees.json'
+    with open(filename, 'w') as file:
+        json.dump(records, file)
