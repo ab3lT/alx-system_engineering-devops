268c038dcdcdc7edcd0d0190a4df892ffb5c1aa8
ab3lT <72024981+ab3lT@users.noreply.github.com>
2022-04-11 22:43:04 +0300

    finding the correct user

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
     import sys
 
     user_req = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                            format(sys.argv[2]))
+                            format(sys.argv[1]))
     user_name = user_req.json().get("name")
     tasks_req = requests.get("https://jsonplaceholder.typicode.com/todos")
     total_tasks = 0
     cmp_tasks = 0
     cmp_tasks_desc = ""
     for each in tasks_req.json():
-        if each["userId"] == int(sys.argv[2]):
+        if each["userId"] == int(sys.argv[1]):
             total_tasks += 1
             if each["completed"] is True:
                 cmp_tasks += 1
